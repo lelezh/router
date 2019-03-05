8b4b96995ce07c9339c783c21aa1c87240b4e525
lelezh <916464326@qq.com>
2019-03-05 16:06:56 +0800

    Add simple code

---
+++ js01.js
@@ -0,0 +1,6 @@
+window.onload=function () {
+    var btn = document.querySelector('button');
+    btn.onclick=function(){
+        document.body.appendChild("<p>hello, first github page demp!</p>");
+    }
+}
